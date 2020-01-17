7a17cbcd44ea4aed4062b984b701f4761ade554c
DavidLiu2-crc <DavidLiu2-crc@users.noreply.github.com>
2020-01-17 15:51:19 -0500

    Update marvinTest.cpp

--- anokiwave-gui/marvin_code/marvinTest.cpp
+++ anokiwave-gui/marvin_code/marvinTest.cpp
@@ -2,7 +2,26 @@
 #include <iostream>
 #include "GTDIO.h"
 
+void CheckStatus(SHORT nStatus) {
+  CHAR sz[512];
+  if (!nStatus) return;
+  DioGetErrorString(nStatus, sz, sizeof sz);
+  std::cout sz;
+  std::cout "Aborting the program...";
+  exit(nStatus);
+}
+
 int main() {
+  SHORT nSlotNum;
+  SHORT nStatus;
+  SHORT nHandle;
+  SHORT nDensity;
+  SHORT nBanks;
+
+  DioSetupInitialization(0, 1, nSlotNum, &nDensity, &nBanks, &nHandle, &nStatus);
+  CheckStatus(nStatus);
+
+
   std::cout << "Hello World!\n" ;
   return 0;
 }
