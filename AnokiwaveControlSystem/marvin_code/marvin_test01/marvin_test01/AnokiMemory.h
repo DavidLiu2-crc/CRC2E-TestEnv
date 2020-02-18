7034bea126579abc682b45a9dee961657345a07c
DavidLiu2crc <DavidLiu2crc@users.noreply.github.com>
2020-02-18 15:36:43 -0500

    Cleaned up VS2019 Github working directory

---
+++ AnokiwaveControlSystem/marvin_code/marvin_test01/marvin_test01/AnokiMemory.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+//#include "AnokiObj.h"
+#include "AnokiCommand.h"
+
+class AnokiMemory {
+
+public:
+	// Command Object to assign the attributes and create the command object
+	AnokiCommand anokiCMD;
+	// Parameter for the PAA frequency [27500 - 30000];
+	unsigned int paramFrequency = 28000;
+	// Parameter for the Beam Mode| 0:Mode 0, 1:Mode 1, 2:Mode 2, 3:Mode 4
+	unsigned int paramBeamMode = 0;
+	// Vector that stores AnokiObj objects
+	std::vector< AnokiObj > nVectorAnokiOBJ;
+
+	// Vector that stores the angles to sweep
+	std::vector< std::vector< float > > nVectorANGLE;
+	// Vector that stores the header information of the csv file
+	std::vector< std::vector< float > > nVectorHeader;
+	// Vector that stores the index of the corresponding command sequence
+	std::vector< unsigned int >			nVectorINDEX;
+
+	// ---------------------------------------------------
+	// Proper indexing types depending on length of command sequence
+	// unsigned char:	[0 - 255 ff]
+	// unsigned short:	[0 - 65 535 ffff]
+	// unsigned int:	[0 - 4 294 967 295 ffffff]
+	// unsigned long long:[0 - 18 446 744 073 709 551 615 ffffffff]
+
+	// float : 3.4E+- 38
+	// double: 1.7E+- 308
+	// ---------------------------------------------------
+
+	
+	// Index counter for max number of command sequences to traverse
+	unsigned int maxCommandSequence = 1000000;
+	// Heap declaration for commandSequence
+	unsigned char* commandSequence = new unsigned char[1000000];
+	// Index counter for where commandSequenceIndex is located currently
+	unsigned int commandSequenceIndex = 0;
+	
+	// Operating card frequency of the object to delay for
+	unsigned int cardFrequency = 1e6;
+	// Calculated number of steps at operating card frequency
+	unsigned int numStepsPAADelay = 0;
+	unsigned int numStepsStrobeBit = 10;
+	unsigned int numStepsScanDelay = 0;
+
+
+	// Relative path of the input angle CSV file
+	char* pnInputCSVFile;
+	bool nInputFileRead = false;
+
+	// Relative path of the output ASCII file
+	char* pnOutputASCIIFile;
+	bool nOutputFileRead = false;
+
+
+
+	// General constructor
+	AnokiMemory() {
+		numStepsPAADelay = 10;
+		numStepsScanDelay = 20;
+	}
+	AnokiMemory(unsigned int _cardFreq) {
+		// Check if the input frequency is reasonably bounded by 100MHz
+		_cardFreq > 1e8 ? cardFrequency = unsigned int(1e8): cardFrequency = _cardFreq;
+
+		// Calculate number of steps to delay for PAA point calculation
+		float delay_PAAPointTime = 12e-6;
+		numStepsPAADelay = unsigned int( ceil(delay_PAAPointTime * cardFrequency) );
+
+		// Calculate number of steps to delay for PXA scanning.
+		float delay_PXAScanTime = 15e-6;
+		numStepsScanDelay = unsigned int( ceil(delay_PXAScanTime * cardFrequency) );
+	}
+
+
+	// --- Begin definition user-defined functions
+
+	/// <summary>
+	/// Opens the .csv file and adds the angles into a (m x n) matrix. Assumes csv file contain 2 column theta, phi values.
+	/// <para>
+	/// Checks if the file refered to by <c>pnInputCSVFile</c> can be opened. If not throw an exception
+	/// Iterates through each line, adding all the characters as float values, then appending to <c>nVectorAngle</c>.
+	/// Sets the <c>nInputFileRead</c> flag as true.
+	/// </para>
+	/// </summary>
+	/// <param name="pnInputCSVFile"> Pointer to a character array refering to the (relative) file path.</param>
+	/// <returns>None
+	/// <para> -Does set nVectorANGLE, nInputFileRead to appropriate values</para></returns>
+	/// <example><code>
+	/// char filePath = 'anglePoint.csv';
+	/// readFromCSV( filePath );
+	/// </code></example>
+	/// <exception cref="FileIOException">If the file is not found in relative directory</exception>
+	void readFromCSV(char* pnInputCSVFile);
+	// Adds calls the PAA command with each line of angle values
+	void cmd_steerAngle();
+	// Adds an anokiObj to nVectorOBJ
+	void set_CurrentCommand(AnokiObj anokiObj);
+	// Adds the enable beam call to nVectorAnokiOBJ
+	void cmd_StartBeam(unsigned int _beamMode, unsigned short _freq);
+	// Adds the disable beam call to nVectorAnokiOBJ
+	void cmd_EndBeam();
+	// Larger function that converts the csv file to raw binary steps
+	void generateCommandSequenceFromFile();
+	// Transposes the command sequence to binary steps
+	void addIndexObjToCommandSequence(int _index);
+	// Appends empty steps to the end of a binary steps to account for delayTime
+	void addDelayStep(unsigned int _numSteps, unsigned char stepValue);
+	// Append appropriate number of steps for PAA point steering timing
+	void addSteerTiming();
+
+	void exportMemoryToASCII();
+
+	// Displays a subsegment of the command sequence binary steps of a certain length from 0
+	void showAnokiCommandSequence(unsigned int _len);
+};
